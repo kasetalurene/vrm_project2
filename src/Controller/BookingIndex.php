7cf48c118685f14558d483421dc0a9a7f49d834c
Rene Kasetalu <rene.kasetalu@khk.ee>
2020-10-24 21:21:27 +0300

    Vormist andmete salvestamine admebaasi

--- src/Controller/BookingIndex.php
+++ src/Controller/BookingIndex.php
@@ -15,12 +15,17 @@ use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Booking;
+
 class BookingIndex extends AbstractController
 {
     /**
      * @Route("/create_booking")
      */
-    public function create_booking()
+    public function create_booking(Request $request)
     {
 
         $form = $this->createFormBuilder()
@@ -65,8 +70,38 @@ class BookingIndex extends AbstractController
             ->add('additionalInformation', TextareaType::class, [
                 'required' => false
             ])
-            ->add('submit', SubmitType::class, ['label' => 'Create Booking'])
+            ->add('submit', SubmitType::class, ['label' => 'Create Bookings'])
             ->getForm();
+        if($request->isMethod('POST')){
+            $form->submit($request->request->get($form->getName()));
+            if($form->isSubmitted() && $form->isvalid()){
+                $data = $form->getData();
+
+                $entityManager = $this->getDoctrine()->getManager();
+
+                $booking = new Booking();
+
+                $booking->setFirstName($data['firstName']);
+                $booking->setLastName($data['lastName']);
+                $booking->setPhone($data['phone']);
+                $booking->setEmail($data['email']);
+                $booking->setBirthday(new \DateTime($data['birthday']->format('Y-m-d')));
+                $booking->setStartDate(new \DateTime($data['startDate']->format('Y-m-d')));
+                $booking->setEndDate(new \DateTime($data['endDate']->format('Y-m-d')));
+                $booking->setArrivalTime(new \DateTime($data['arrivalTime']->format('H:i:s')));
+                $booking->setnrOfPeople($data['nrOfPeople']);
+                $booking->setPayingMethod($data['payingMethod']);
+                $booking->setAdditionalInformation($data['additionalInformation']);
+
+                $entityManager->persist($booking);
+
+                $entityManager->flush();
+
+                return $this->redirectToRoute('bookings');
+            }
+        }
+
+
 
         return $this->render('bookings/create_booking.html.twig', [
             'form' => $form->createView(),
@@ -74,7 +109,7 @@ class BookingIndex extends AbstractController
     }
 
     /**
-     * @Route("/bookings")
+     * @Route("/bookings", name="bookings")
      */
     public function bookings()
     {
